6e92645f5a6b46f866af8c2424098c626030b81a
Mark Baggett <mbagget1@utk.edu>
2019-11-28 22:16:37 -0600

    Refactor constructor and add get_all_repos.
    Since httpclient allows you to pass headers as a part of the
    HttpClient type, let's remove headers from ArchivesSpace type and
    just track the authentication header in ArchivesSpace.client.

--- src/archivesspace.nim
+++ src/archivesspace.nim
@@ -5,17 +5,17 @@ type
     ## This type describes an ArchivesSpace request.
     base_url*: string
     client: HttpClient
-    headers: string
 
 proc newArchivesSpace*(url: string="http://localhost:8089", user: string="admin", password: string="admin"): ArchivesSpace =
   let client = newHttpClient()
   let json_data = parseJson(client.post(url & "/users/" & user & "/login?password=" & password).body)
   let session = json_data["session"].getStr()
-  let headers = $(%{
-    "X-ArchivesSpace-Session": %session
-  }) & "\c\l"
-  ArchivesSpace(base_url: url, client: client, headers: headers)
+  client.headers = newHttpHeaders({"X-ArchivesSpace-Session": session})
+  ArchivesSpace(base_url: url, client: client)
+
+method get_all_repositories*(this: ArchivesSpace): string {. base .} =
+  this.client.get(this.base_url & "/repositories").body
 
 when isMainModule:
   var x = newArchivesSpace()
-  echo x.headers
+  echo x.get_all_repositories()
